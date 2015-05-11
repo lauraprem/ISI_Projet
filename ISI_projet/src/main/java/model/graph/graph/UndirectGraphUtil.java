a77dea3f00e6e17997ab2509ef58243d6bd8a3fe
agaldeano <alexandre.galdeano@etu.univ-lyon1.fr>
2015-05-11 17:20:19 +0200

    [evol] Ajout d'une fonction qui renvoie la liste des Nodes en feu, test passed.

--- ISI_projet/src/main/java/model/graph/graph/UndirectGraphUtil.java
+++ ISI_projet/src/main/java/model/graph/graph/UndirectGraphUtil.java
@@ -14,4 +14,7 @@ public class UndirectGraphUtil {
     public static List<Node> getUnlinkedNodes(IGraph graph) {
         return graph.getAllNodes().stream().filter(node -> !node.isLinked()).collect(Collectors.toList());
     }
+    public static List<Node> getOnFireNodes(IGraph graph) {
+        return graph.getAllNodes().stream().filter(node -> !node.isOnFire()).collect(Collectors.toList());
+    }
 }
