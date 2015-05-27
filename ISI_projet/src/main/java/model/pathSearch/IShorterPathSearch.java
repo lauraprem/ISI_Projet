b74acdce4d830d91e0e265418db680e96adc32da
agaldeano <alexandre.galdeano@etu.univ-lyon1.fr>
2015-05-27 15:33:12 +0200

    [evol] Le shorter path return la distance et set le nodePAth

--- ISI_projet/src/main/java/model/pathSearch/IShorterPathSearch.java
+++ ISI_projet/src/main/java/model/pathSearch/IShorterPathSearch.java
@@ -1,25 +1,27 @@
 package model.pathSearch;
 
-import java.util.List;
-
 import model.graph.Node;
 import model.graph.graph.IGraph;
-import model.graph.ground.Ground;
+import model.graph.ground.GroundType;
+import model.robot.NodePath;
+
+import java.util.List;
 
 /**
  * permet de trouver le plus court chemin
- * @author Laura
  *
+ * @author Laura
  */
 public interface IShorterPathSearch {
 
-	/**
-	 * Permet de trouver le plus court chemin sur un graph d'un point de départ jusqu'à un point d'arrivée
-	 * @param graph sur lequel on se déplace
-	 * @param start noeud de départ
-	 * @param goal noeud destination
-	 * @param capacity : terrain sur lesquels on peut passer
-	 * @return la liste des noeuds qui composent le plus court chemin si le noeud est atteignable, liste vide si le noeud n'est pas atteignable
-	 */
-	public List<Node> findShorterPath(IGraph graph, Node start, Node goal, List<Ground> capacity);
+    /**
+     * Permet de trouver le plus court chemin sur un graph d'un point de départ jusqu'à un point d'arrivée
+     *
+     * @param graph    sur lequel on se déplace
+     * @param start    noeud de départ
+     * @param goal     noeud destination
+     * @param capacity : terrain sur lesquels on peut passer
+     * @return la liste des noeuds qui composent le plus court chemin si le noeud est atteignable, liste vide si le noeud n'est pas atteignable
+     */
+    Float findShorterPath(IGraph graph, Node start, Node goal, List<GroundType> capacity, NodePath nodePath);
 }
