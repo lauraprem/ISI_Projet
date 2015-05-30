aee6a754af784d98480a7276d215f1c22d011a10
agaldeano <alexandre.galdeano@etu.univ-lyon1.fr>
2015-05-30 11:21:41 +0200

    [evol] Nouveau test edge, nouveau equals ground

--- ISI_projet/src/test/java/model/graph/edge/EdgeTest.java
+++ ISI_projet/src/test/java/model/graph/edge/EdgeTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Alexandre
@@ -15,11 +16,13 @@ import static org.junit.Assert.assertEquals;
  */
 public class EdgeTest {
     private Edge edge;
+    private Node source = new Node("test1", new Point(1, 2));
+    private Node destination = new Node("test2", new Point(-1, -2));
 
     @Before
     public void setUp() throws Exception {
-        edge = new Edge(new Node("test1", new Point(1, 2)),
-                new Node("test2", new Point(-1, -2)),
+        edge = new Edge(source.clone(),
+                destination.clone(),
                 3.0,
                 new Ground(GroundType.FLAT, 1.0));
     }
@@ -33,5 +36,15 @@ public class EdgeTest {
                 new Ground(GroundType.FLAT, 1.0));
         edge.updateGround();
         assertEquals(String.format("Ground type is %s should be %s", edge.getGround().getType(), expected), expected, edge.getGround().getType());
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+        Edge edge2 = new Edge(source.clone(),
+                destination.clone(),
+                3.0,
+                new Ground(GroundType.FLAT, 1.0));
+        assertTrue(edge2.equals(edge));
+
     }
 }
