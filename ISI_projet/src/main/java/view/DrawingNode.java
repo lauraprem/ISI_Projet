9cd9a9a8006cc51248f71428ce90020b2eaf9bc5
FaCoInf <corinne.fagno@gmail.com>
2015-06-08 22:12:45 +0200

    [Vue] : arrangement

--- ISI_projet/src/main/java/view/DrawingNode.java
+++ ISI_projet/src/main/java/view/DrawingNode.java
@@ -1,19 +1,8 @@
 package view;
-// package logo;
 
 import model.graph.Node;
 
 import java.awt.*;
-
-/**
- * Titre :        Logo
- * Description :  Un exemple de programme graphique utilisant la celebre Tortue Logo
- * Copyright :    Copyright (c) 2000
- * Societe :      LIRMM
- *
- * @author J. Ferber
- * @version 2.0
- */
 
 public class DrawingNode {
 
